98d03dc600061e4f98fc94a8872369d9f37e20ac
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2022-02-08 18:38:16 -0300

    Requisito 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,7 @@
 // Coloque aqui suas actions
+export const SAVE_USER = 'SAVE_USER';
+
+export const saveUserAction = (payload) => ({
+  type: SAVE_USER,
+  payload,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,81 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { saveUserAction } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      isDisabled: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const emailRegex = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.com$/;
+    const minLength = 6;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    }, () => {
+      const { email, password } = this.state;
+      if (emailRegex.test(email) && password.length >= minLength) {
+        this.setState({ isDisabled: false });
+      } else {
+        this.setState({ isDisabled: true });
+      }
+    });
+  }
+
+  handleClick() {
+    const { history, saveUser } = this.props;
+    const { email } = this.state;
+    saveUser(email);
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, isDisabled } = this.state;
+    return (
+      <div>
+        <input
+          type="email"
+          name="email"
+          value={ email }
+          data-testid="email-input"
+          onChange={ this.handleChange }
+        />
+        <input
+          type="password"
+          name="password"
+          value={ password }
+          data-testid="password-input"
+          onChange={ this.handleChange }
+        />
+        <button
+          type="submit"
+          disabled={ isDisabled }
+          onClick={ this.handleClick }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  saveUser: (email) => dispatch(saveUserAction(email)),
+});
+
+Login.propTypes = {
+  history: PropTypes.objectOf(PropTypes.any.isRequired).isRequired,
+  saveUser: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
