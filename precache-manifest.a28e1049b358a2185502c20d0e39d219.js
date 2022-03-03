4d31e62a9bd7cab5d5848142d78613610b27669f
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2022-03-02 22:55:35 -0300

    github pages

---
+++ precache-manifest.a28e1049b358a2185502c20d0e39d219.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "741f87d205ec0f2f3ef4f83851e42780",
+    "url": "/ProjectTrybeWallet/index.html"
+  },
+  {
+    "revision": "8b20ad87957fac1b71c3",
+    "url": "/ProjectTrybeWallet/static/css/main.436bf0c3.chunk.css"
+  },
+  {
+    "revision": "7e0b441798c8c8952b69",
+    "url": "/ProjectTrybeWallet/static/js/2.724f0896.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ProjectTrybeWallet/static/js/2.724f0896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b20ad87957fac1b71c3",
+    "url": "/ProjectTrybeWallet/static/js/main.162fecc6.chunk.js"
+  },
+  {
+    "revision": "3b9b83042d8477ccec97",
+    "url": "/ProjectTrybeWallet/static/js/runtime-main.8e1d03ee.js"
+  },
+  {
+    "revision": "99884b4da52098594cbd1099371c149b",
+    "url": "/ProjectTrybeWallet/static/media/Money-Pattern.99884b4d.webp"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ProjectTrybeWallet/precache-manifest.a28e1049b358a2185502c20d0e39d219.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ProjectTrybeWallet/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
